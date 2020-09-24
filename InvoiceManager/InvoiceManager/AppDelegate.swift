b2e7d9e24973c90ba4f808e108771c7736416d5a
Tianid <tianid73@gmail.com>
2020-09-24 20:43:52 +0300

    Added coredata stack, added preloaded data for coredata, preparations for coredata

--- InvoiceManager/InvoiceManager/AppDelegate.swift
+++ InvoiceManager/InvoiceManager/AppDelegate.swift
@@ -7,16 +7,19 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-    
+    var container: NSPersistentContainer!
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         setupRoot()
+        setupCoreDataStack()
+
         return true
     }
     
@@ -40,8 +43,84 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         testView.testData = testBills1
         let tets = UITabBarController()
         tets.viewControllers = [testView]
-        window?.rootViewController = tabBar
+        window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
+    }
+    
+    private func setupCoreDataStack() {
+        self.container = createContainer()
+        let context = container.viewContext
+        let root = self.window?.rootViewController
+        let vRoot = root as? ViewController
+        vRoot?.context = context
+        self.preloadData()
+    }
+    
+    private func createContainer() -> NSPersistentContainer? {
+        let container = NSPersistentContainer(name: "Modelv0.1")
+        container.loadPersistentStores { (_, error) in
+            guard error == nil else {
+                fatalError("Failed to load store")
+            }
+        }
+        
+        return container
+    }
+    
+    
+    private func preloadData() {
+        let preloadedDataKey = "didPreloadData"
+        let userDefaults = UserDefaults.standard
+        if userDefaults.bool(forKey: preloadedDataKey) == false {
+            guard let urlPath = Bundle.main.url(forResource: "PreloadedData", withExtension: "plist") else { return }
+            let data = try! Data(contentsOf: urlPath)
+            
+            guard let plist = try! PropertyListSerialization.propertyList(from: data, options: .mutableContainers, format: nil) as? [String: [String]] else { return }
+            
+            let backgroundContext = container.newBackgroundContext()
+            container.viewContext.automaticallyMergesChangesFromParent = true
+            
+            var keys = Array(plist.keys)
+            keys.sort()
+            
+            backgroundContext.perform {
+                for section in keys {
+                    let categorys = plist[section]
+                    let sectionCD = CDSection(context: backgroundContext)
+                    sectionCD.name = section
+                    
+                    for category in categorys! {
+                        let categoryCD = CDCategory(context: backgroundContext)
+                        categoryCD.name = category
+                        categoryCD.iconImageName = "NO IMAGE"
+                        sectionCD.addToCategory(categoryCD)
+                    }
+                }
+                
+                do {
+                    try backgroundContext.save()
+                    userDefaults.set(true, forKey: preloadedDataKey)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    private func getAllCategorys() {
+        let backgroundContext = container.newBackgroundContext()
+        let request = NSFetchRequest<CDCategory>(entityName: "\(CDCategory.self)")
+        let data = try! backgroundContext.fetch(request)
+        
+        
+        for iten in data {
+            print(iten.name)
+            print(iten.iconImageName)
+            print(iten.section?.name)
+            
+            print()
+            
+        }
     }
 }
 
--- InvoiceManager/InvoiceManager/ViewController.swift
+++ InvoiceManager/InvoiceManager/ViewController.swift
@@ -7,9 +7,24 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    var context: NSManagedObjectContext! {
+        didSet {
+            
+            let requeset = NSFetchRequest<CDSection>(entityName: "\(CDSection.self)")
+            let sections = try! context.fetch(requeset)
+            
+            let requesetCategory = NSFetchRequest<CDCategory>(entityName: "\(CDCategory.self)")
+            let category = try! context.fetch(requesetCategory)
+            
+            coreDataSections = category
+            tableView.reloadData()
+        }
+    }
+    
     private let tableIdentifier = "tableViewCell"
     private var tableView: UITableView = {
         let table = UITableView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), style: .plain)
@@ -63,19 +78,29 @@ class ViewController: UIViewController {
         //        text.addTarget(self, action: #selector(showCategoryActionSheet(_:)), for: .touchDown)
         return text
     }()
-     var testData: [Bill]? = testBills1
+    
+    private var addButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Add data", for: .normal)
+        button.addTarget(self, action: #selector(addNewDataToCD), for: .touchUpInside)
+        return button
+    }()
+    
+    var testData: [Bill]? = testBills1
+    var coreDataSections: [CDCategory] = [CDCategory]()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .orange
 //        view.addSubview(dropMenu)
-        view.addSubview(nameTextField)
+//        view.addSubview(nameTextField)
 //        view.addSubview(valueTextField)
 //        view.addSubview(categoryTextField)
-        dropMenu.dataSourse = [.EUR, .GBP, .RUB, .USD]
-        nameTextField.delegate = self
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(test), name: UIResponder.keyboardWillShowNotification, object: nil)
+//        dropMenu.dataSourse = [.EUR, .GBP, .RUB, .USD]
+//        nameTextField.delegate = self
+//
+//        NotificationCenter.default.addObserver(self, selector: #selector(test), name: UIResponder.keyboardWillShowNotification, object: nil)
         
 //        dropMenu.anchor(top: view.safeAreaLayoutGuide.topAnchor,
 //                        leading: view.safeAreaLayoutGuide.leadingAnchor,
@@ -83,7 +108,7 @@ class ViewController: UIViewController {
         
 //        dropMenu.anchor(size: CGSize(width: 100, height: 0), centerX: view.centerXAnchor)
         
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideDropMenu)))
+//        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideDropMenu)))
         
         
 //        nameTextField.anchor(top: view.safeAreaLayoutGuide.topAnchor,
@@ -93,14 +118,14 @@ class ViewController: UIViewController {
         
 
         
-        nameTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant:  50).isActive = true
-        nameTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant:  8).isActive = true
-        nameTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant:  -8).isActive = true
-        nameTextField.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+//        nameTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant:  50).isActive = true
+//        nameTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant:  8).isActive = true
+//        nameTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant:  -8).isActive = true
+//        nameTextField.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
 
 
         
-
+//
 //        valueTextField.anchor(top: nameTextField.bottomAnchor,
 //                              leading: view.safeAreaLayoutGuide.leadingAnchor,
 //                              trailing: view.safeAreaLayoutGuide.trailingAnchor,
@@ -113,27 +138,56 @@ class ViewController: UIViewController {
         
         
         
-//        view.addSubview(tableView)
-//        view.addSubview(someView)
-//        someView.addSubview(dismissButton)
-//
-//        someView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
-//                        leading: view.safeAreaLayoutGuide.leadingAnchor,
-//                        trailing: view.safeAreaLayoutGuide.trailingAnchor,
-//                        size: CGSize(width: 0, height: 300))
-//
-//        tableView.anchor(top: someView.safeAreaLayoutGuide.bottomAnchor,
-//                         leading: view.safeAreaLayoutGuide.leadingAnchor,
-//                         bottom: view.safeAreaLayoutGuide.bottomAnchor,
-//                         trailing: view.safeAreaLayoutGuide.trailingAnchor,
-//                         padding: UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8))
-//
-//        dismissButton.anchor(top: someView.topAnchor,
-//                             leading: someView.leadingAnchor,
-//                             trailing: someView.trailingAnchor,
-//                             size: CGSize(width: 0, height: 50))
-//
-//        configureTableVIew()
+        
+        
+        view.addSubview(tableView)
+        view.addSubview(someView)
+        someView.addSubview(dismissButton)
+        someView.addSubview(addButton)
+
+        someView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                        leading: view.safeAreaLayoutGuide.leadingAnchor,
+                        trailing: view.safeAreaLayoutGuide.trailingAnchor,
+                        size: CGSize(width: 0, height: 100))
+
+        tableView.anchor(top: someView.safeAreaLayoutGuide.bottomAnchor,
+                         leading: view.safeAreaLayoutGuide.leadingAnchor,
+                         bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                         trailing: view.safeAreaLayoutGuide.trailingAnchor,
+                         padding: UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8))
+
+        dismissButton.anchor(top: someView.topAnchor,
+                             leading: someView.leadingAnchor,
+                             trailing: someView.trailingAnchor,
+                             size: CGSize(width: 0, height: 50))
+        
+        addButton.anchor(top: dismissButton.bottomAnchor,
+                         leading: dismissButton.leadingAnchor,
+                         trailing: dismissButton.trailingAnchor,
+                         size: CGSize(width: 0, height: 50))
+
+        configureTableVIew()
+        
+    }
+    
+    @objc func addNewDataToCD() {
+        let category = NSEntityDescription.insertNewObject(forEntityName: "\(CDCategory.self)", into: context) as? CDCategory
+        
+        category?.name = "category NAME1 \(Int.random(in: 0...1000))"
+        category?.iconImageName = "iconImage NAME1 \(Int.random(in: 0...1000))"
+        
+        
+        let section = NSEntityDescription.insertNewObject(forEntityName: "\(CDSection.self)", into: context) as? CDSection
+        
+        section?.name = "section NAME1 \(Int.random(in: 0...1000))"
+        
+        category?.section = section
+        
+        do {
+            try context.save()
+        } catch {
+            print(error)
+        }
         
     }
     
@@ -159,20 +213,26 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return testData?.count ?? 0
+        return coreDataSections.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: tableIdentifier, for: indexPath) as? HomeViewTableViewCell
-        guard let data = testData?[indexPath.row] else { return UITableViewCell() }
-        cell?.billNameLabel.text = data.billName
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "d MMM yyyy HH:mm"
-        cell?.billDateLabel.text = dateFormatter.string(from: data.modifiedDate)
-        cell?.billCategoryLabel.text = data.category.name
-        cell?.billValueLabel.text = String(data.value)
-        //        cell.textLabel?.text = String(indexPath.row)
+        cell?.billNameLabel.text = coreDataSections[indexPath.row].name
+        cell?.billCategoryLabel.text = coreDataSections[indexPath.row].iconImageName
+        cell?.billDateLabel.text = coreDataSections[indexPath.row].section?.name
         return cell!
+        
+        
+//        guard let data = testData?[indexPath.row] else { return UITableViewCell() }
+//        cell?.billNameLabel.text = data.billName
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "d MMM yyyy HH:mm"
+//        cell?.billDateLabel.text = dateFormatter.string(from: data.modifiedDate)
+//        cell?.billCategoryLabel.text = data.category.name
+//        cell?.billValueLabel.text = String(data.value)
+//        //        cell.textLabel?.text = String(indexPath.row)
+//        return cell!
     }
 }
 
